26f8ed4fc80b54a97437c7c42dbc59bf427b7ef4
Gary <garyko24@yahoo.com>
2021-05-24 19:41:12 +0300

    added date selector

--- stocktracker.py
+++ stocktracker.py
@@ -8,11 +8,11 @@ start_date_from_user = st.date_input("choose start date: ")
 stop_date_from_user = st.date_input("choose stop date:")
 st.write('Shown are the stock closing price and volume of',ticker_from_user,'!')
 tickerSymbol = ticker_from_user
+
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start=start_date_from_user, end=stop_date_from_user)
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
 
