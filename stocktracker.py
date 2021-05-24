7ebc3c803a7c995d494cdd799a73ae5847464ae3
Gary <garyko24@yahoo.com>
2021-05-24 19:28:54 +0300

    added date selector

--- stocktracker.py
+++ stocktracker.py
@@ -6,12 +6,14 @@ import streamlit as st
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 ticker_from_user = st.text_input("Enter the stock ticker: ")
+start_date_from_user = st.date_input("choose start date: ")
+stop_date_from_user = st.date_input("choose stop date:")
 st.write('Shown are the stock closing price and volume of',ticker_from_user,'!')
 tickerSymbol = ticker_from_user
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-5-24')
+tickerDf = tickerData.history(period='1d', start=start_date_from_user, end=stop_date_from_user)
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
