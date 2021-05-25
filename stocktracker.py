44572efc99f937c2b312342acfad304e8dec7dfa
Gary <garyko24@yahoo.com>
2021-05-25 11:19:49 +0300

    implemented new features

--- stocktracker.py
+++ stocktracker.py
@@ -1,18 +1,28 @@
 import yfinance as yf
 import streamlit as st
+from PIL import Image
 
 
-#define the ticker symbol
-ticker_from_user = st.text_input("Enter the stock ticker: ")
-start_date_from_user = st.date_input("choose start date: ")
-stop_date_from_user = st.date_input("choose stop date:")
-st.write('Shown are the stock closing price and volume of',ticker_from_user,'!')
+stonks_image = st.sidebar.image('money.png')
+ticker_from_user = st.sidebar.text_input("Enter the stock ticker: ")
+start_date_from_user = st.sidebar.date_input("Choose start date: ")
+stop_date_from_user = st.sidebar.date_input("Choose stop date: ")
+
+
+# define the ticker symbol
+st.write('Shown are the stock closing price and volume of', ticker_from_user, '!')
 tickerSymbol = ticker_from_user
 
-#get data on this ticker
+# get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start=start_date_from_user, end=stop_date_from_user)
+# get the historical prices for this ticker
+tickerDf = tickerData.history(
+    period='1d', start=start_date_from_user, end=stop_date_from_user)
 
 st.line_chart(tickerDf.Close)
+print("\n")
 
+# get earnings data and display on side bar
+earnings_from_yfinance = tickerData.earnings
+earnings_by_ticker = st.sidebar.write(ticker_from_user, "earnings data from Yahoo: ")
+recommendations_side_bar = st.sidebar.write(earnings_from_yfinance)
